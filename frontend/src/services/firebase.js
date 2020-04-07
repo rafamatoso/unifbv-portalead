37feb0c56b581f196dff13c9ddfded0341655873
Noe Oliveira <noe.oliveira1995@gmail.com>
2020-04-07 06:58:27 -0300

    Adicionado o firebase

---
+++ frontend/src/services/firebase.js
@@ -0,0 +1,22 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+export default app
+
+export const database = firebase.firestore();
+
+export const storage = firebase.storage();
+
+export const auth = firebase.auth();
