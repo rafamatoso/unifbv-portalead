27bbd74ae69b961bf2e6169b6009070de63c2e40
Rafael Matoso <rafaelmatosog@gmail.com>
2020-05-08 22:44:24 -0300

    Config eslintrc and prettierrc

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,25 +2,38 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ["airbnb", "prettier", "prettier/react"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+    __DEV__: "readonly",
   },
+  parser: "babel-eslint",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 11,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ["react", "prettier", "jest"],
   rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "no-use-before-define": ["error", { variables: false }],
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".jsx", ".js"],
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "import/no-extraneous-dependencies": "off",
   },
 };
