8b5c440c9145a6107820247566b0e371fc654e94
Rafael Matoso <rafaelmatosog@gmail.com>
2020-05-09 22:00:05 -0300

    chore/Add lint-staged

--- frontend/__tests__/app.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-
-import App from "../src/App";
-
-it("Hello Jest", () => {
-  renderer.create(<App></App>);
-});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import React, { useReducer } from "react";
-import * as Store from "./store";
-import { Routes } from "./routes";
+import React, {useReducer} from 'react';
+import * as Store from './store';
+import {Routes} from './routes';
 
-import "./App.css";
+import './App.css';
 
 export default function App() {
   const [store, dispatch] = useReducer(Store.reducer, Store.initalState);
-
   return (
-    <Store.Context.Provider value={{ store, dispatch }}>
+    <Store.Context.Provider value={{store, dispatch}}>
       <Routes />
+      <p string="aaa"> </p>
     </Store.Context.Provider>
   );
 }
