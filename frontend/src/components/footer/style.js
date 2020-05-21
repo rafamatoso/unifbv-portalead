1ff217ec7e8c0df49a10e67cc32a78bc173fcd5a
isaeljj <44845107+isaeljj@users.noreply.github.com>
2020-05-20 21:42:37 -0300

    Update style.js

--- frontend/src/components/footer/style.js
+++ frontend/src/components/footer/style.js
@@ -4,8 +4,6 @@ import { colors } from '../../utils/colors';
 
 export const useStyles = makeStyles((theme) => ({
   footer: {
-    width: '100%',
-    position: 'fixed',
     backgroundColor: colors.blue1,
     padding: theme.spacing(3),
     zIndex: theme.zIndex.drawer + 1,
